f66f49bd646f42c510b571ddb4bb146fb848aaba
RhymesR <allmicandfame@gmail.com>
2026-05-16 11:11:49 +0100

    fix: the right url for render

--- app/email-check/scanner.tsx
+++ app/email-check/scanner.tsx
@@ -37,7 +37,7 @@ const Scanner = () => {
 
         try {
             // 1. Start scan
-            const res = await fetch(`/api/scan/email`, {
+            const res = await fetch(`/api/scan`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
